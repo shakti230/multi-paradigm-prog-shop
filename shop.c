9d857d19c113725024361f3b8f3eb4a90844b92e
shakti230 <36244887+shakti230@users.noreply.github.com>
2019-10-14 16:48:09 +0100

    Adding more entities

--- shop.c
+++ shop.c
@@ -26,13 +26,20 @@ struct Customer {
     struct ProductStock shoppintList[10];
 };
 
+void printProduct(Product p)
+{
+    printf("---------------------\n");
+    printf("PRODUCT NAME: %s \n PRODUCT PRICE: \n %.2f\n", p.name, p.price);
+    printf("---------------------\n");
+}
+
 int main(void)
 {
    struct Customer Mary = {"Mary", 100.0};
    printf("Customer name is %s\n", Mary.name);
 
    struct Product coke = { "Can Coke", 1.10 };
-   printf("The %s costs %.2f\n", coke.name, coke.price);
+   printProduct(coke);
     
    struct ProductStock cokeStock= { coke, 20 };
    printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
